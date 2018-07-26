af039ec781aaeaaeddd7b9fc47104cba83d400b8
nieyan0723 <nieyan0723@gmail.com>
2018-07-25 22:22:59 -0700

    plz4

--- index.js
+++ index.js
@@ -1,53 +1,74 @@
-'use strict'
+'use strict';
 
-var express = require('express')
-var bodyParser = require('body-parser')
-var request = require('request')
+let Wit = null;
+let interactive = null;
+try {
+  // if running from repo
+  Wit = require('../').Wit;
+  interactive = require('../').interactive;
+} catch (e) {
+  Wit = require('node-wit').Wit;
+  interactive = require('node-wit').interactive;
+}
 
-var Config = require('./config')
-var FB = require('./connectors/facebook')
-var Bot = require('./bot')
-
-
-// LETS MAKE A SERVER!
-var app = express()
-app.set('port', (process.env.PORT) || 5000)
-// SPIN UP SERVER
-app.listen(app.get('port'), function () {
-  console.log('Running on port', app.get('port'))
-})
-// PARSE THE BODY
-app.use(bodyParser.json())
-
-
-// index page
-app.get('/', function (req, res) {
-  res.send('hello world i am a chat bot')
-})
-
-// for facebook to verify
-app.get('/webhooks', function (req, res) {
-  if (req.query['hub.verify_token'] === Config.FB_VERIFY_TOKEN) {
-    res.send(req.query['hub.challenge'])
+const accessToken = (() => {
+  if (process.argv.length !== 3) {
+    console.log('usage: node examples/joke.js <wit-access-token>');
+    process.exit(1);
   }
-  res.send('Error, wrong token')
-})
+  return process.argv[2];
+})();
 
-// to send messages to facebook
-app.post('/webhooks', function (req, res) {
-  var entry = FB.getMessageEntry(req.body)
-  // IS THE ENTRY A VALID MESSAGE?
-  if (entry && entry.message) {
-    if (entry.message.attachments) {
-      // NOT SMART ENOUGH FOR ATTACHMENTS YET
-      FB.newMessage(entry.sender.id, "That's interesting!")
+// Joke example
+// See https://wit.ai/aforaleka/wit-example-joke-bot/
+
+const allJokes = {
+  chuck: [
+    'Chuck Norris counted to infinity - twice.',
+    'Death once had a near-Chuck Norris experience.',
+  ],
+  tech: [
+    'Did you hear about the two antennas that got married? The ceremony was long and boring, but the reception was great!',
+    'Why do geeks mistake Halloween and Christmas? Because Oct 31 === Dec 25.',
+  ],
+  default: [
+    'Why was the Math book sad? Because it had so many problems.',
+  ],
+};
+
+const firstEntityValue = (entities, entity) => {
+  const val = entities && entities[entity] &&
+    Array.isArray(entities[entity]) &&
+    entities[entity].length > 0 &&
+    entities[entity][0].value
+  ;
+  if (!val) {
+    return null;
+  }
+  return typeof val === 'object' ? val.value : val;
+};
+
+const handleMessage = ({entities}) => {
+  const getJoke = firstEntityValue(entities, 'getJoke');
+  const greetings = firstEntityValue(entities, 'greetings');
+  const category = firstEntityValue(entities, 'category');
+  const sentiment = firstEntityValue(entities, 'sentiment');
+  if (getJoke) {
+    if (category) {
+      const jokes = allJokes[category];
+      console.log(jokes[Math.floor(Math.random() * jokes.length)]);
     } else {
-      // SEND TO BOT FOR PROCESSING
-      Bot.read(entry.sender.id, entry.message.text, function (sender, reply) {
-        FB.newMessage(sender, reply)
-      })
+      console.log(allJokes['default'][0]);
     }
+  } else if (sentiment) {
+    const reply = sentiment === 'positive' ? 'Glad you liked it.' : 'Hmm.';
+    console.log(reply);
+  } else if (greetings) {
+    console.log("hey this is joke bot :)");
+  } else {
+    console.log("I can tell jokes! Say 'tell me a joke about tech'!");
   }
+};
 
-  res.sendStatus(200)
-})
+const client = new Wit({accessToken});
+interactive(client, handleMessage);
