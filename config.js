68adf03b4219e742ba5d7d6e2198d1a4cdab5428
nieyan0723 <nieyan0723@gmail.com>
2018-07-26 20:15:45 -0700

    fbjdi

--- config.js
+++ config.js
@@ -6,12 +6,12 @@ if (!WIT_TOKEN) {
 }
 
 
-var FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN 
+var FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN || 'EAAEfrRktRMkBAEfvq5ujBpFGq7huui9yiqASVfctZC7eXYZCCNBaAS1PD8r5eytWUKLilhoEtogZAE2OgURCMEtadTGalasLZApxh4QmLxRi3j8bFjSyjf9tiR3Xemchk7D0MxXZC3CZCtElRcueiWmpGH74uodVR8kfcNWrmrHZAi2MdplVUSf'
 if (!FB_PAGE_TOKEN) {
 	throw new Error('Missing FB_PAGE_TOKEN. Go to https://developers.facebook.com/docs/pages/access-tokens to get one.')
 }
 
-var FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN 
+var FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN || 'just_do_it'
 
 module.exports = {
   WIT_TOKEN: WIT_TOKEN,
