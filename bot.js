17eee49becf91ee2525e12c12890a6cbba8cded3
nieyan0723 <nieyan0723@gmail.com>
2018-07-26 22:19:40 -0700

    1qghjj

--- bot.js
+++ bot.js
@@ -45,7 +45,7 @@ const printWikidataDescription = (celebrity) => {
     .then(response => Promise.resolve(response.json()))
     .then(data => {
       console.log(`ooo yes I know ${celebrity.name} -- ${data.entities[wikidataID].descriptions.en.value}`);
-      console.log(data);
+      return "ooo yes I know " + celebrity.name + " -- " + data.entities[wikidataID].descriptions.en.value;
     })
     .catch(err => console.error(err))
 };
